92306c31aae3989582caca35b8731899f5117df3
livaage <liv.helen.vage@cern.ch>
2025-02-26 08:20:57 -0500

    change graph storage from networkx to torch

--- input_proc/lst_to_pyg.py
+++ input_proc/lst_to_pyg.py
@@ -6,10 +6,11 @@ from tqdm import tqdm
 from concurrent.futures import ProcessPoolExecutor, as_completed
 from torch_geometric.data import Data
 from argparse import ArgumentParser
+import time 
 
 NODE_FEATURES = ["MD_0_x", "MD_0_y", "MD_0_z", "MD_0_r", "MD_1_x", "MD_1_y", "MD_1_z", "MD_1_r", "MD_eta", "MD_phi",
                  "MD_pt", "MD_dphichange"]
-EDGE_FEATURES = ["LS_pt", "LS_eta", "LS_phi", "LS_dphichange"]
+EDGE_FEATURES = ["LS_pt", "LS_eta", "LS_phi"]
 EDGE_INDEX = ["LS_MD_idx0", "LS_MD_idx1"]
 Y_VAL = ["LS_isFake"]
 
@@ -43,27 +44,17 @@ class GraphBuilder:
         print(f"Saved graph {idx}")
 
     def process_events_in_parallel(self, n_workers, debug=False):
-        """Processes events in parallel with a progress tracker."""
-        num_events = self.input_tree.num_entries
-
-        if debug:
-            # Process first event for debugging
-            event_data = ak.to_dataframe(self.input_tree.arrays(entry_start=0, entry_stop=1))
-            self.process_event(event_data, 0, debug)
-            return
-
+        num_events = self.input_tree.num_entries if not debug else 1
+        t1 = time.time()
         with ProcessPoolExecutor(max_workers=n_workers) as executor:
-            futures = {}
-            with tqdm(total=num_events, desc="Processing Events", dynamic_ncols=True) as pbar:
                 for idx in range(num_events):
-                    event_data = ak.to_dataframe(self.input_tree.arrays(entry_start=idx, entry_stop=idx+1))
-                    futures[executor.submit(self.process_event, event_data, idx)] = idx
+                    executor.submit(
+                    self.process_event,
+                    ak.to_dataframe(self.input_tree.arrays(entry_start=idx, entry_stop=idx + 1)),
+                    idx)
 
-                for future in as_completed(futures):
-                    result = future.result()
-                    print(result)  # Print progress/errors immediately
-                    pbar.update(1)  # Update progress bar dynamically
-
+        t2 = time.time() 
+        print("time", t2-t1)
 if __name__ == "__main__":
     argparser = ArgumentParser()
     argparser.add_argument("--input_path", type=str, required=True)
